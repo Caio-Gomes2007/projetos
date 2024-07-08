126268ed173ebab2b1c3ce574acd553978a79566
Caio Gomes <caiofgomesrj@gmail.com>
2024-07-08 19:50:15 -0300

    up

--- php/textos/php/cadastrar.php
+++ php/textos/php/cadastrar.php
@@ -1,17 +1,25 @@
 <?php
-include "connection.php";
+require "connection.php";
 
-$nome = $_POST['nome'];
-$email = $_POST['email'];
-$password = $_POST['password'];
+if (isset($_POST['nome'])) {
+    if (
+        $_POST['nome'] != null ||
+        $_POST['email'] != null
+        || $_POST['password'] != null
+    ) {
+        $nome = $_POST['nome'];
+        $email = $_POST['email'];
+        $password = $_POST['password'];
 
-$sql = "INSERT INTO usuarios(nome, email, senha)
+        $sql = "INSERT INTO usuarios(nome, email, senha)
 VALUES ('$nome','$email','$password');";
 
-if ($conn->query($sql) === TRUE) {
-    echo "New record created successfully";
-    //add cookies e session
-} else {
-    echo "Error: " . $sql . "<br>" . $conn->error;
-};
-header("Location: ../Html/paginaInicial.html");
+        if ($conn->query($sql) === TRUE) {
+            echo "New record created successfully";
+            //add cookies e session
+        } else {
+            echo "Error: " . $sql . "<br>" . $conn->error;
+        };
+        header("Location: ../Html/paginaInicial.html");
+    }
+}
